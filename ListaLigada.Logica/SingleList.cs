c6768f4cfbc82522c8813bd2b1a78c5bf6b40c58
SebastianRSS04 <22030619@itesa.edu.mx>
2024-10-28 08:45:13 -0600

    Version_02: Metodo Agregar elemento y To String

---
+++ ListaLigada.Logica/SingleList.cs
@@ -0,0 +1,51 @@
+﻿
+namespace ListaLigada.Logica
+{
+    public class SingleList <T>
+    {
+        private SingleNode<T>? _first;
+
+        public SingleList()
+        {
+            _first = null;
+        }
+
+        public bool isEmmpy => _first == null;
+
+        public void Add(T item) 
+        {
+            var node = new SingleNode<T>(item);
+            if (isEmmpy)
+            {
+                _first = node;
+            }
+            else
+            { 
+                var pointer = _first;
+
+                while (pointer!.Next != null)
+                {
+                    pointer= pointer.Next;  
+                }
+
+                pointer!.Next = node;
+            }
+
+        }
+
+        public override string ToString()
+        {
+            var cadenaLista = string.Empty;
+            var Pointer = _first;
+
+            while (Pointer != null)
+            {
+                cadenaLista += $"{Pointer.Data}\n";  
+                Pointer = Pointer.Next;
+            }
+            return cadenaLista; 
+        }
+
+   
+    }
+}
--- ListaLigada.UI.Consola/Program.cs
+++ ListaLigada.UI.Consola/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ListaLigada.Logica;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!, Listas Ligadas");
+
+        Console.WriteLine("");
+
+        var singleList = new SingleList<String>();
+
+        singleList.Add("Jose");
+        singleList.Add("Sebastian");
+        singleList.Add("Rodriguez");
+        singleList.Add("Salgado");
+        singleList.Add("ISC");
+
+        Console.WriteLine(singleList);
+
+
+        Console.ReadKey();
+    }
+}
