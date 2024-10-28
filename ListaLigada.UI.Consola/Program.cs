d4af21e617ce3a9de67a62202c5da688884aa8bc
SebastianRSS04 <22030619@itesa.edu.mx>
2024-10-28 09:17:25 -0600

    Version_03: Se agrego el menu de opciones.

--- ListaLigada.UI.Consola/Program.cs
+++ ListaLigada.UI.Consola/Program.cs
@@ -1,4 +1,5 @@
 ﻿using ListaLigada.Logica;
+using System.ComponentModel.Design;
 
 internal class Program
 {
@@ -10,15 +11,82 @@ internal class Program
 
         var singleList = new SingleList<String>();
 
-        singleList.Add("Jose");
-        singleList.Add("Sebastian");
-        singleList.Add("Rodriguez");
-        singleList.Add("Salgado");
-        singleList.Add("ISC");
+        int opcion = 0;
+        do
+
+        {
+            opcion = Menu();
+
+            switch (opcion) 
+            {
+                case 1: AgregarElemento();
+                    break;
+                case 2: MostrarLista(); break;
+
+                case 3: break;
+
+                default: Console.WriteLine("Esta opcion no exite"); 
+                    break;
+            
+            }
+        }
+        while (opcion != 0);
 
         Console.WriteLine(singleList);
+
+        //Metodo agregar
+
+        void AgregarElemento()
+        {
+            Console.WriteLine("\n ----------------------- \n");
+            Console.WriteLine("Escribe el nombre del nodo \n");
+            var data = Console.ReadLine();
+            singleList.Add(data);
+        }
+
+        void MostrarLista() 
+        { 
+            Console.WriteLine("La lista es: \n");
+            Console.WriteLine(singleList);    
+        
+        }
+
 
 
         Console.ReadKey();
     }
+
+    
+
+    private static int Menu()
+    {
+        Console.WriteLine("\n ----------------------- \n");
+        Console.WriteLine("Menu \n");
+        Console.WriteLine("1. Agregar elemento \n");
+        Console.WriteLine("2. Monstrar la Lista \n");
+        Console.WriteLine("0. Salir \n");
+
+        bool esValido = false;
+
+        int opcion = 0;
+
+        do
+        {
+            Console.WriteLine("Elije una opcion del menu: \n");
+            var opcionCapturada = Console.ReadLine();
+
+            if (!int.TryParse(opcionCapturada, out opcion) || opcion < 0 || opcion > 2)
+            {
+                Console.WriteLine("Dijite una opcion valida.");
+                esValido = false;
+            }
+            else 
+            { 
+                esValido = true;    
+            }
+        }
+        while (esValido == false);
+
+        return opcion; 
+    } 
 }
